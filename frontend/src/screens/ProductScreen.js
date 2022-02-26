ac1975d0d68e8d7923623f7788e5537e6ac1eedd
aichaabdelali <aicha.abdelali6@gmail.com>
2022-02-26 19:54:56 +0100

    nodemon concurrently and environment variables setup

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,21 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, ListGroup, Image, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
 
 const ProductScreen = () => {
   const params = useParams();
-  const product = products.find((p) => p._id === params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${params.id}`);
+
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [params]);
   return (
     <>
       <Link className="btn btn-dark my-3" to="/">
